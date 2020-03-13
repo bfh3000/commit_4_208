9ed9e84f9de34563286d342e4e97a20646096170
bfh3000 <bfh3000@naver.com>
2020-03-13 09:19:32 +0900

    564654

--- src/programmers_test/lv0_2020Kakao_1.java
+++ src/programmers_test/lv0_2020Kakao_1.java
@@ -1,10 +1,33 @@
 package programmers_test;
-
+import java.util.*;
 public class lv0_2020Kakao_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println();
+		solution(null);
+	}
+
+	private static String solution(String p) {
+			p = "()))((()";	 
+			int a = division(p);
+			String w = ""; // 올바른 정렬값
+			 String a = "aa"; // p의 값에서 올바른 정렬값을 뺀 밸류
+			 for(int i=0; i<p.length(); i++) {
+				 
+				 if(p.charAt(0) == '(') {
+					 for
+					 
+				 }else {
+					 
+				 }
+				 
+				  
+			 }
+			    
+			    
+			    
+			    	String answer = "";
+			        return answer;
 	}
 
 }
+
