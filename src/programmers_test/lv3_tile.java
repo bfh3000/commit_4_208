d629cf90d4cd1cbffcab09754edb3234b68988b0
bfh3000 <bfh3000@naver.com>
2020-03-17 02:27:26 +0900

    213123

--- src/programmers_test/lv3_tile.java
+++ src/programmers_test/lv3_tile.java
@@ -4,7 +4,13 @@ public class lv3_tile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//1이면 (1+1+1+1) 	= 2  + 2 = 4
+		//2이면 (2+2+1+1) 	= 4  + 2 = 6
+		//3이면 (3+3+2+2) 	= 6  + 4 = 10
+		//4이면 (5+5+3+3) 	= 10 + 6 = 16
+		//5이면 (8+8+5+5) 	= 16 + 10 = 26
+		//6이면 (13+13+8+8) 	= 26 + 16 = 42
+		
 	}
 
 }
