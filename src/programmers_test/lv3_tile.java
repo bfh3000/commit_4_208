de5809d4e54ce43c96761fa110744f50223c3a72
bfh3000 <bfh3000@naver.com>
2020-03-19 03:56:53 +0900

    dd

--- src/programmers_test/lv3_tile.java
+++ src/programmers_test/lv3_tile.java
@@ -3,24 +3,41 @@ package programmers_test;
 public class lv3_tile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//1이면 (1+1+1+1) 	= 2  + 2 = 4
 		//2이면 (2+2+1+1) 	= 4  + 2 = 6
 		//3이면 (3+3+2+2) 	= 6  + 4 = 10
 		//4이면 (5+5+3+3) 	= 10 + 6 = 16
 		//5이면 (8+8+5+5) 	= 16 + 10 = 26
 		//6이면 (13+13+8+8) 	= 26 + 16 = 42
-		int a = 1;
-		solution(a);
+		int test = 4;
+		int solution = solution(test);
+		System.out.println(solution);
 	}
-
-	private static void solution(int a) {
-		// TODO Auto-generated method stub
-		for(int i=1; i<=a; i++){
-			for(int j=1; j<=a; j++){
-				
+	private static int solution(int a) {
+		int sum = 0;
+		int pre_sum1 = 0;
+		int pre_sum2 = 0;
+		int i = 1;
+			while(i <= a) {
+				if(i==1) {
+					pre_sum1 = 2;
+					pre_sum2 = 2;
+					sum = pre_sum1 + pre_sum2;
+					pre_sum1 = sum;
+					pre_sum2 = pre_sum1;
+				}else if(i==2){
+					sum = pre_sum1 + pre_sum2;
+					pre_sum1 = sum;
+					pre_sum2 = pre_sum1;
+				}else {
+					sum = pre_sum1 + pre_sum2;
+					pre_sum1 = sum;
+					pre_sum2 = pre_sum1;
+					
+				}
+				i++;
 			}
-		}
+		return sum;
 		
 	}
 
