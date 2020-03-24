24b9731f1fdd08f2527bff791dccbcf8685e86af
bfh3000 <bfh3000@naver.com>
2020-03-24 22:16:13 +0900

    222

---
+++ src/programmers_test/lv2_2018Kakao_2.java
@@ -0,0 +1,15 @@
+package programmers_test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class lv2_2018Kakao_2 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static void solution(int[] priorities, int location) {
+	}
+}
