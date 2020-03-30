49717cb7e844a7bef4b406f78c728e2d8c1d6948
bfh3000 <bfh3000@naver.com>
2020-03-30 10:05:02 +0900

    2222

---
+++ src/programmers_test/lv2_.java
@@ -0,0 +1,14 @@
+package programmers_test;
+
+public class lv2_ {
+
+	public static void main(String[] args) {
+		System.out.println();
+	}
+	private static int solution(int a) {
+		return 0;
+		
+	}
+
+}
+
