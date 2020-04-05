1aa695e22fa0d4af3d8518844f6d5e0ed3d85b6a
bfh3000 <bfh3000@naver.com>
2020-04-05 20:17:13 +0900

    ㅇㅇㅇ

---
+++ src/programmers_test/lv3_airport.java
@@ -0,0 +1,13 @@
+package programmers_test;
+
+public class lv3_airport {
+
+	public static void main(String[] args) {
+		System.out.println(solution);
+	}
+	private static int solution(int a) {
+	//이번주는 문제 안했네	
+	}
+
+}
+
