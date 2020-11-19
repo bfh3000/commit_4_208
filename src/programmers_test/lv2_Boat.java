0f89c489218ee35ea6d280fea54f4a50101b717f
bfh3000 <bfh3000@naver.com>
2020-11-19 09:32:10 +0900

    asdkjfkasdjf

--- src/programmers_test/lv2_Boat.java
+++ src/programmers_test/lv2_Boat.java
@@ -13,8 +13,7 @@ public class lv2_Boat {
 	        int ship = 0; 
 	        int first = 0; // 첫번
 	        int last = people.length-1;
-
-	        
+////////	        
 	        while(first<=last) {
 	            //제한을 초과하지 않는다면, 충족하기 때문에 두 사람을 뺀다. 첫번째빼는 조건으로 first++을 넣는다.  
 	        	if(people[first] + people[last] <= limit) {
