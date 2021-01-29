7d4cf4df32c1a72baef9b932ec1db0116093c58e
bfh3000 <bfh3000@naver.com>
2021-01-29 16:06:11 +0900

    ddf231

--- src/programmers_test/lv0_2020Kakao_1.java
+++ src/programmers_test/lv0_2020Kakao_1.java
@@ -7,6 +7,8 @@ public class lv0_2020Kakao_1 {
 	public static void main(String[] args) {
 		//값 대입 후 테스트.
 		//값 대입 후 테스트.2
+		//값 대입 후 테스트.2
+		//값 대입 후 테스트.3
 		System.out.println(solution("())("));
 	}
     public static String solution(String p) {
