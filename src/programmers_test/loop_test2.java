12c2525860ae8070af67acf61f2dd60e294235af
bfh3000 <bfh3000@naver.com>
2021-03-27 09:38:15 +0900

    a2

--- src/programmers_test/loop_test2.java
+++ src/programmers_test/loop_test2.java
@@ -10,7 +10,6 @@ public class loop_test2 {
 		String[] test_list2 = new String[]{"1", "2", "5"};
 		String[] test_list3 = new String[]{"1", "2", "2"};
 		String[] a;
-		String[] b2;
 		String[] b3;
 		List<String> same_list_value = new ArrayList<String>();
 		for(int i=0; i<test_list.length; i++){
