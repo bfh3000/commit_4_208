5bb5fc7bfcec5bce520cbb52ea437b3aa50c98ae
bfh3000 <bfh3000@naver.com>
2021-05-18 10:40:33 +0900

    ooaodsoo2

--- src/programmers_test/lv0_2020Kakao_1.java
+++ src/programmers_test/lv0_2020Kakao_1.java
@@ -8,7 +8,7 @@ public class lv0_2020Kakao_1 {
 		//값 대입 후 테스트.
 		//값 대입 후 테스트.3
 		System.out.println(solution("())("));
-		
+		System.out.println(solution("())("));
 		System.out.println(solution("())("));
 		System.out.println(solution("())("));
 	}
