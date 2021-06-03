dd60c79f44980cbf9818f4f65aa23de63d6535a0
bfh3000 <bfh3000@naver.com>
2021-06-03 20:53:52 +0900

    44

---
+++ src/tistory/java.java
@@ -0,0 +1,29 @@
+package tistory;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class java {
+	public static void main(String[] args) {
+
+		Map<Integer, String> Map = new HashMap<Integer, String>();
+
+		Map.put( 22, "uuuuuuu");
+        Map.put( 15, "aaaaaaa");
+        Map.put( 44, "ddddddd");
+        Map.put( 11, "ccccccc");
+        Map.put( 16, "iiiiiii");
+        Map.put( 31, "ttttttt");
+        Map.put( 42, "bbbbbbb");
+
+        Object[] map_key = Map.keySet().toArray();
+        Arrays.sort(map_key);
+
+        // 결과 출력
+        for (Integer nKey : Map.keySet())
+        {
+            System.out.println(Map.get(nKey));
+        }
+    }
+}
